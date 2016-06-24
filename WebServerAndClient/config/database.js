1a7ecfd592f96506de50c02cd5b0d6b763cadc34
jason <appleoxxo@gmail.com>
2016-06-24 18:40:06 +0800

    多個檔案上傳、google帳號登入並取得暱稱和圖片

---
+++ WebServerAndClient/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://root:0000@ds055564.mlab.com:55564/ascrum' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
